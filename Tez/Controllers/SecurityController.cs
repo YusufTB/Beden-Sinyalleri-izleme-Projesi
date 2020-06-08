d4ae0962b39b182652c12d0c9e6c0ad7b8ad5391
YusufTB <yutaba1903@hotmail.com>
2020-06-08 18:58:20 +0300

    commit4

--- Tez/Controllers/SecurityController.cs
+++ Tez/Controllers/SecurityController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using Google.Cloud.Firestore;
 using Tez.Models;
 using static Tez.Models.User;
 
@@ -11,6 +12,16 @@ namespace Tez.Controllers
 {
     public class SecurityController : Controller
     {
+        FirestoreDb db = FirestoreDb.Create("alzheimertakip-e1d1e");
+
+        DocumentReference docRef = db.Collection("users").Document("alovelace");
+        Dictionary<string, object> user = new Dictionary<string, object>
+        {
+            { "First", "Ada" },
+            { "Last", "Lovelace" },
+            { "Born", 1815 }
+        };
+        await docRef.SetAsync(user);
         // GET: Security
         public ActionResult Login(string username)
         {
@@ -32,10 +43,10 @@ namespace Tez.Controllers
         }
         public ActionResult Logout()
         {
-            
-             FormsAuthentication.SignOut();
-             
-            
+
+            FormsAuthentication.SignOut();
+
+
             return Redirect("/Security/Login");
         }
     }
