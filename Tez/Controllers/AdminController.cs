f61bb232d6ee1345dcb239d04408dbd08571aa1b
futi7 <fuatseven1997@hotmail.com>
2020-06-15 16:36:04 +0300

    Added notes and update pofile

---
+++ Tez/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tez.Controllers
+{
+    public class AdminController : Controller
+    {
+        FirestoreDb db;
+        // GET: Admin
+        [Authorize]
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            var username = System.Web.HttpContext.Current.User.Identity.Name;
+            db = FirestoreDb.Create("alzheimertakip-e1d1e");
+            DocumentReference docRef = db.Collection("Hasta").Document(username);
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+            Dictionary<string, object> user = snapshot.ToDictionary();
+            if (user["role"].ToString() == "A")
+            {
+                ViewBag.username = username;
+                return View();
+            }
+            else
+                return Redirect("/Home/Index");
+        }
+        [Authorize]
+        public async System.Threading.Tasks.Task<ActionResult> AdminView(string hastaismi)
+        {
+            var username = System.Web.HttpContext.Current.User.Identity.Name;
+            db = FirestoreDb.Create("alzheimertakip-e1d1e");
+            DocumentReference docRef = db.Collection("Hasta").Document(username);
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+            Dictionary<string, object> user = snapshot.ToDictionary();
+            if (user["role"].ToString() == "A")
+            {
+                ViewBag.username = hastaismi;
+                return View();
+            }
+            else
+                return Redirect("/Home/Index");
+        }
+        [Authorize]
+        public ActionResult AdminDashboardView(string hastaismi)
+        {
+            ViewBag.username = "Yusuf";
+            return View();
+        }
+    }
+}
